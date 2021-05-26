098b352e1819d24a6e3a388ca99e915affce5f26
Shaodi.kou <79649931@qq.com>
2021-05-26 15:04:32 +0800

    feat: 将模块通过nacos注册到注册中心上

--- gulimall-coupon/src/main/java/icu/kandx/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/icu/kandx/gulimall/coupon/GulimallCouponApplication.java
@@ -3,7 +3,10 @@ package icu.kandx.gulimall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+// 将服务注册到nacos
+@EnableDiscoveryClient
 @MapperScan("icu.kandx.gulimall.coupon.dao")
 @SpringBootApplication
 public class GulimallCouponApplication {
